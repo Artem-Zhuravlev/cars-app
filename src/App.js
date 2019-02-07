670d6d219ffd16bdf56ea574b8d2de47bedbd705
Artem Zhuravlev <mstrJesm@gmail.com>
2019-02-07 14:34:31 +0200

    add new method onChangeTitle in Car.js component

--- src/App.js
+++ src/App.js
@@ -12,9 +12,9 @@ class App extends Component {
     pageTitle: 'React components'
   }
 
-  changeTitleHandler = () => {
+  changeTitleHandler = newTitle => {
     this.setState({
-      pageTitle: 'Well done'
+      pageTitle: newTitle
     })
   }
 
@@ -29,9 +29,15 @@ class App extends Component {
     return (
       <div style={divStyle}>
         <h1>{this.state.pageTitle}</h1>
-        <button onClick ={this.changeTitleHandler}>Change title</button>
-        <Car name={cars[0].name} year={cars[0].year}/>
-        <Car name={cars[1].name} year={cars[1].year}/>
+        <button onClick = {this.changeTitleHandler.bind(this, 'Changed')}>Change title</button>
+        <Car 
+          name={cars[0].name} 
+          year={cars[0].year}
+          onChangeTitle = {this.changeTitleHandler.bind(this, cars[0].name)}/>
+        <Car 
+          name={cars[1].name} 
+          year={cars[1].year}
+          onChangeTitle = {this.changeTitleHandler.bind(this, cars[1].name)}/>
         
       </div>
     );
