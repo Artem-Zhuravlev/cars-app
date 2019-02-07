d951c32dffb0eada554120ccaa69006fad7747a3
Artem Zhuravlev <mstrJesm@gmail.com>
2019-02-07 12:11:46 +0200

    add changeTitleHandler method in App.js

--- src/App.js
+++ src/App.js
@@ -12,6 +12,12 @@ class App extends Component {
     pageTitle: 'React components'
   }
 
+  changeTitleHandler = () => {
+    this.setState({
+      pageTitle: 'Well done'
+    })
+  }
+
   render() {
     const divStyle = {
       textAlign: "center",
@@ -23,6 +29,7 @@ class App extends Component {
     return (
       <div style={divStyle}>
         <h1>{this.state.pageTitle}</h1>
+        <button onClick ={this.changeTitleHandler}>Change title</button>
         <Car name={cars[0].name} year={cars[0].year}/>
         <Car name={cars[1].name} year={cars[1].year}/>
         
